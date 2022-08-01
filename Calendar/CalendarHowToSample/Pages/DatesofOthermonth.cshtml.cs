734246580ebfab9d7aa454444521fd800b048ff3
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2022-08-01 17:38:44 +0530

    Prepared Calendar UG samples

---
+++ Calendar/CalendarHowToSample/Pages/DatesofOthermonth.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CalendarHowToSample.Pages
+{
+    public class DatesofOthermonthModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Calendar/CalendarUGSample/Pages/DayCellFormat.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CalendarUGSample.Pages
+{
+    public class DayCellFormatModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
