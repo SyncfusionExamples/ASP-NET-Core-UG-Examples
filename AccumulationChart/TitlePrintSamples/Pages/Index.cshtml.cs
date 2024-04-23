2753f43e3670de09f8a0fd17575fff027ebe53c7
Backiaraj <backiaraj.kanagaraj@syncfusion.com>
2024-04-23 18:00:48 +0530

    Prepared UG Title and SubTitle samples for AccumulationChart control

---
+++ AccumulationChart/TitlePrintSamples/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TitlePrintSamples.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public class PieChartData
+        {
+            public string xValue;
+            public double yValue;
+            public string text;
+        }
+    }
+}
